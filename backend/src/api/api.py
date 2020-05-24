d8cb30366ddf60738c433f5d5db2d1565be62677
kavinraju <rajukavin25@gmail.com>
2020-05-24 10:06:41 +0530

    feat: Implemented api endpoints.

---
+++ backend/src/api/api.py
@@ -0,0 +1,259 @@
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+
+from ..database.models import setup_db, Actor, Movie
+from ..app import app
+
+""" POST """
+# Post actors and movies
+
+@app.route('/actor', methods=['POST'])
+def create_actor():
+    body = request.get_json()
+    print('body ', body)
+    if not body:
+        abort(422)
+    else:
+        name = body.get('name', None)
+        gender = body.get('gender', None)
+        age = body.get('age', None)
+
+        try:
+            new_actor = Actor(name=name, age=age, gender=gender)
+            new_actor.insert()
+
+            actors = Actor.query.all()
+            actors_formated = [actor.format() for actor in actors]
+
+            return jsonify({
+                'success': True,
+                'added': new_actor.id,
+                'actors': actors_formated,
+                'total_actors': len(actors_formated)
+            })
+        except Exception as e:
+            return jsonify({
+                'success':  False,
+                'error': str(e)
+            })
+
+@app.route('/movie', methods=['POST'])
+def create_movie():
+    body = request.get_json()
+    print('body ', body)
+    if not body:
+        abort(422)
+    else:
+        title = body.get('title', None)
+        release_date = body.get('release_date', None)
+        actors = body.get('actors', None)
+
+        try:
+            new_movie = Movie(title=title, release_date=release_date)
+            
+            for actor in actors:
+                print(actor)
+                actor_in_db = Actor.query.filter_by(id=actor).one_or_none()
+
+                if actor_in_db:
+                    new_movie.actors.append(actor_in_db)
+            
+            new_movie.insert()
+
+            movies = Movie.query.all()
+            movies_formated = [movie.format() for movie in movies]
+
+            return jsonify({
+                'success': True,
+                'added': new_movie.id,
+                'movies': movies_formated,
+                'total_movies': len(movies_formated)
+            })
+        except Exception as e:
+            return jsonify({
+                'success':  False,
+                'error': str(e)
+            })
+
+""" GET """
+# Get actors of movie_id
+
+@app.route('/movie/<int:movie_id>/actors',methods=['GET'])
+def get_actors_of_a_movie(movie_id):
+    movie = Movie.query.filter_by(id=movie_id).one_or_none()
+    if movie is None:
+        return jsonify({
+            'success': False
+        })
+    else:
+        actors = movie.actors
+        actors_formated = [actor.format() for actor in actors]
+        return jsonify({
+            'success': True,
+            'actors': actors_formated
+        })
+
+# Get movies
+@app.route('/movie', methods=['GET'])
+def get_movies():
+    try:
+        movies = Movie.query.order_by(Movie.id).all()
+        movies_formated = [movie.format() for movie in movies]
+
+        return jsonify({
+            'success': True,
+            'movies': movies_formated,
+            'total_movies': len(movies)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+# Get actors
+@app.route('/actor', methods=['GET'])
+def get_actors():
+    try:
+        actors = Actor.query.order_by(Actor.id).all()
+        actors_formated = [actor.format() for actor in actors]
+
+        return jsonify({
+            'success': True,
+            'actors': actors_formated,
+            'total_actors': len(actors)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+""" PATCH """
+# Update movie details
+@app.route('/movie/<int:movie_id>', methods=['PATCH'])
+def update_movie(movie_id):
+    try:
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+
+        if movie is None:
+            abort(404)
+        else:
+            body = request.get_json()
+            title = body.get('title', None)
+            release_date = body.get('release_date', None)
+            actors = body.get('actors', None)
+
+
+            if title is not None:
+                movie.title = title
+            if release_date is not None:
+                movie.release_date = release_date
+            if actors is not None:
+                movie.actors = []
+                for actor_id in actors:
+                    print(actor_id, 'df')
+                    actor_in_db = Actor.query.filter_by(id=actor_id).one_or_none()
+                    print(actor_in_db)
+                    if actor_in_db:
+                        movie.actors.append(actor_in_db)
+
+            movie.update()
+
+            movies = Movie.query.all()
+
+        return jsonify({
+            'success': True,
+            'updated_movie': movie.format(),
+            'total_movies': len(movies)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+# Update actor details
+@app.route('/actor/<int:actor_id>', methods=['PATCH'])
+def update_actor(actor_id):
+    try:
+        actor = Actor.query.filter_by(id=actor_id).one_or_none()
+
+        if actor is None:
+            abort(404)
+        else:
+            body = request.get_json()
+            name = body.get('name', None)
+            age = body.get('age', None)
+            gender = body.get('gender', None)
+
+            if name is not None:
+                actor.name = name
+            if age is not None:
+                actor.age = age
+            if name is not None:
+                actor.gender = gender
+
+            actor.update()
+
+            actors = Actor.query.all()
+
+        return jsonify({
+            'success': True,
+            'updated_actor': actor.format(),
+            'total_actors': len(actors)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+""" DELETE """
+@app.route('/actor/<int:actor_id>', methods=['DELETE'])
+def delete_actor(actor_id):
+    try:
+        actor = actor = Actor.query.filter_by(id=actor_id).one_or_none()
+        if actor is None:
+            abort(404)
+        else:
+            actor.delete()
+            actors = Actor.query.all()
+        return jsonify({
+            'success': True,
+            'deleted_actor': actor_id,
+            'total_actors': len(actors)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+@app.route('/movie/<int:movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+    try:
+        movie = movie = Movie.query.filter_by(id=movie_id).one_or_none()
+        if movie is None:
+            abort(404)
+        else:
+            movie.delete()
+            movies = Movie.query.all()
+        return jsonify({
+            'success': True,
+            'deleted_movie': movie_id,
+            'total_movies': len(movies)
+        })
+    except Exception as e:
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        })
+
+
+@app.route('/')
+def index():
+    return jsonify({
+        'message': 'HOME PAGE'
+    })
--- backend/src/app.py
+++ backend/src/app.py
@@ -6,3 +6,6 @@ from .database.models import setup_db
 
 app = Flask(__name__)
 setup_db(app)
+
+""" Importing api endpoint from app/api.py """
+from .api.api import *
